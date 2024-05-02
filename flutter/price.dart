a342e79e16a5570f08ae75b4e0bbd7e563d9b7a1
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-02 17:22:29 +0300

    Update price.dart

--- flutter/price.dart
+++ flutter/price.dart
@@ -6,10 +6,8 @@ class Price extends Car {
 
   Price(this._price) : super("");
 
-  // Method to get the price
   String getPrice() => _price;
 
-  // Method to print the price and car info
   void carPrice() {
     print("The price of car is: ${getPrice()}");
     super.showCarInfo();
