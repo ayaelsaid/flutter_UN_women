b41dfe2bf0e28fd89cec6ebe450d3626140591bb
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-02 17:57:52 +0300

    Update maincar.dart

--- flutter/maincar.dart
+++ flutter/maincar.dart
@@ -3,12 +3,33 @@ import 'class_car.dart';
 import 'price.dart';
 
 void main() {
-  var myCar = Car("red", manufactureYear: "2020", motorSpeed: "40 k/h");
+  var myCar = Option('Blue', model: 'SUV', manufactureYear: '2019', motorSpeed: '180');
+  var myOption = Option('Black',
+      manufactureYear: '2019',
+      motorSpeed: '180',
+      model: 'SUV',
+      fuelEfficiency: '20 mpg',
+      safetyFeatures: 'Airbags');
+
+  print('myCar Information:');
   myCar.showCarInfo();
+  print('my car option');
+  myOption.showCarInfo();
+  var myNewOption = Option('Black', model: 'SUV');
+  print("=======================");
+  myNewOption.showCarInfo();
 
-  var option = Option("Toyota Camry", "30 mpg", "ABS, Airbags");
-  option.showCarInfo(); 
+  var myCarPrice =
+      Price('Red', manufactureYear: '2021', motorSpeed: '220', price: '50000');
+  myCarPrice.carPrice();
+  print("==========================");
+  var myNewCarPrice = Price('Red', price: '50000');
+  myNewCarPrice.carPrice();
 
-  var myCarPrice = Price("250000");
-  myCarPrice.carPrice(); 
+  var myNewCar = Car("red", manufactureYear: "2020", motorSpeed: "40 k/h");
+  myNewCar.showCarInfo();
+  var myOwn = Car("red");
+  print("=======================================");
+  myOwn.showMotorSpeed();
+  myOwn.showCarInfo();
 }
