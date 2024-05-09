c5f6b052fc4e0b80a2fae56df7390c922e5f993c
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-09 18:56:45 +0300

    Update main.dart

--- flutter_application_1/main.dart
+++ flutter_application_1/main.dart
@@ -18,55 +18,77 @@ void main() {
             decoration: BoxDecoration(
               color: Colors.white,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      color: Colors.orange,
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'assets/download.jpeg',
-                            fit: BoxFit.contain,
-                            scale: 2,
-                          ),
-                          Text('Vegetables Pizza'),
-                        ],
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/download.jpeg',
+                              fit: BoxFit.contain,
+                              scale: 2,
+                            ),
+                            SizedBox(width: 10),
+                            Text('Vegetables Pizza'),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 100),
-                    Container(
-                      color: Colors.orange,
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'assets/download(1).jpeg',
-                            fit: BoxFit.contain,
-                            scale: 2,
-                          ),
-                          Text('Cheese Pizza'),
-                        ],
+                      SizedBox(height: 100),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/download(1).jpeg',
+                                  fit: BoxFit.contain,
+                                  scale: 2,
+                                ),
+                                SizedBox(width: 10),
+                                Text('Cheese Pizza'),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 100),
-                    Container(
-                      color: Colors.orange,
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'assets/download(2).jpeg',
-                            fit: BoxFit.contain,
-                            scale: 2,
-                          ),
-                          Text('Box of Fries'),
-                        ],
+                      SizedBox(height: 100),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/download(2).jpeg',
+                                  fit: BoxFit.contain,
+                                ),
+                                SizedBox(width: 10),
+                                Text('Box of Fries'),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
