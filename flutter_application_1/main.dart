eb55d61aa35a9b0bc35a1f6e713a214829625d83
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-16 02:31:12 +0300

    Update main.dart

--- flutter_application_1/main.dart
+++ flutter_application_1/main.dart
@@ -1,125 +1,127 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      left: 10),
-                  child: Text(
-                    "WOW Pizza",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                },
-                icon: Image.asset(
-                  'assets/twitter.png',
-                  fit: BoxFit.contain,
-                  height: 50,
-                  width: 50,
-                ),
-              ),
-              SizedBox(width: 1),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      right: 10),
-                  child: Center(
-                    child: IconButton(
-                      onPressed: () {
-                      },
-                      icon: Icon(Icons.facebook, size: 50),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          height: 800,
+          width: 500,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(15),
           ),
-          backgroundColor: Colors.orange,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          padding: EdgeInsets.all(10),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  getButtonCard('Vegetable Pizza'),
-                  getButtonCard('Cheese Pizza'),
-                  getButtonCard('Fries'),
-                ],
-              ),
-              Center(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 40),
-                      child: Container(
-                        margin: EdgeInsets.all(5),
-                        width: double.infinity,
+                  Column(
+                    children: [
+                      Container(
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                                                    color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        child: Image.asset(
-                          'assets/pizaaf.jpg',
-                          fit: BoxFit.cover,
-                          width: double.maxFinite,
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/download.jpeg',
+                              fit: BoxFit.contain,
+                              scale: 2,
+                            ),
+                            SizedBox(width: 10),
+                            Text('Vegetables Pizza'),
+                          ],
                         ),
                       ),
-                    ),
-                    Text(
-                      'Hi, I\'m the Pizza Assistant,\nwhat can I help you order?',
-                      style: TextStyle(fontSize: 30, color: Colors.black),
-                    ),
-                  ],
-                ),
+                      SizedBox(height: 100),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/download(1).jpeg',
+                                  fit: BoxFit.contain,
+                                  scale: 2,
+                                                                  ),
+                                SizedBox(width: 10),
+                                Text('Cheese Pizza'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 100),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/download(2).jpeg',
+                                  fit: BoxFit.contain,
+                                ),
+                                SizedBox(width: 10),
+                                Text('Box of Fries'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                                                      ),
+                                SizedBox(width: 10),
+                                Text('Cheese Pizza'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 100),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/download(2).jpeg',
+                                  fit: BoxFit.contain,
+                                ),
+                                SizedBox(width: 10),
+                                Text('Box of Fries'),
+                                                            ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget getButtonCard(String myText) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.all(10),
-          backgroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.horizontal(
-              left: Radius.circular(40),
-              right: Radius.circular(40),
             ),
           ),
         ),
-        onPressed: () {},
-        child: Text(
-          myText,
-          style: const TextStyle(
-            color: Colors.orange,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
       ),
-    );
-  }
+    ),
+  ));
 }
