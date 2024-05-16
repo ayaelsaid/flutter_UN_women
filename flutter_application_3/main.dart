42ebcd457918445e9a8d36ea6cf85e9f020bdb53
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-16 20:32:40 +0300

    Add files via upload

--- flutter_application_3/main.dart
+++ flutter_application_3/main.dart
@@ -1,23 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_3/pages/cheese_pizza.dart';
-import 'package:flutter_application_3/pages/fries.dart';
-import 'package:flutter_application_3/pages/home.dart';
-import 'package:flutter_application_3/pages/veagetable_pizza.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'card.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        (ctx) => HomePage(),
-        (ctx) => CheesePizza(),
-        (ctx) => VegetablePizza(),
-        (cTX) => Fries()
-      },
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
-
