87015ae2fb89ac8efdba075bfc530bf596efc1da
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-05-16 20:33:24 +0300

    Update main.dart

--- flutter_application_3/main.dart
+++ flutter_application_3/main.dart
@@ -1,8 +1,37 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'card.dart';
+import 'package:flutter_application_3/pages/cheese_pizza.dart';
+import 'package:flutter_application_3/pages/fries.dart';
+import 'package:flutter_application_3/pages/home.dart';
+import 'package:flutter_application_3/pages/veagetable_pizza.dart';
+import 'package:flutter_application_3/wedgets/web_widget.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.white,
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context) => const HomePage());
+          case '/cheesePizza':
+            return MaterialPageRoute(builder: (context) => const CheesePizza());
+          case '/vegetablePizza':
+            return MaterialPageRoute(builder: (context) => const VegetablePizza());
+          case '/fries':
+            return MaterialPageRoute(builder: (context) => const Fries());
+          case '/webView':
+            return MaterialPageRoute(builder: (context) => const WebViewPage());
+          default:
+            return null;
+        }
+      },
+    );
+  }
 }
